8552f1975e2c55baf2024a37412cc1dd7215c04a
ChrisMagnuson <hourglasssand@gmail.com>
2016-07-20 13:51:46 -0400

    Install function to setup scheduled task, dynamic response variables type and name,

--- CamundaExternalServiceTaskPowerShellRouter.psm1
+++ CamundaExternalServiceTaskPowerShellRouter.psm1
@@ -2,11 +2,49 @@
 #Requires -Version 5
 
 $TopicNamesToGetExternalTasksFor = @"
-Get-ADUserSAMAccountNameFromName
+Get-BPMNADUserSAMAccountNameFromName
 Disable-ADAccount
 "@ -split "`r`n"
 
 $TimeToLockTasksInMS = 1000
+
+function Install-CamundaExternalServiceTaskPowerShellRouter {
+    param(
+        [parameter(Mandatory)]$CamundaServer,
+        $PathToScriptForScheduledTask = $PSScriptRoot,
+        [parameter(Mandatory)]$ScheduledTaskUserPassword
+    )
+    
+    [Environment]::SetEnvironmentVariable("CamundaServer", $CamundaServer, "User")
+
+@"
+Invoke-CamundaExternalServiceTaskPowerShellRouting
+"@ | Out-File "$PathToScriptForScheduledTask\Invoke-CamundaExternalServiceTaskPowerShellRouting.ps1"
+
+
+    $ScriptFilePath = "$PathToScriptForScheduledTask\Invoke-CamundaExternalServiceTaskPowerShellRouting.ps1"
+
+    $ScheduledTaskAction = New-ScheduledTaskAction –Execute "Powershell.exe" -Argument "-noprofile -file $ScriptFilePath"
+    $ScheduledTaskTrigger = New-ScheduledTaskTrigger -Daily -At 12am
+    $ScheduledTaskSettingsSet = New-ScheduledTaskSettingsSet
+    $Task = Register-ScheduledTask -TaskName "Invoke-CamundaExternalServiceTaskPowerShellRouting" `
+                    -TaskPath "\" `
+                    -Action $ScheduledTaskAction `
+                    -Trigger $ScheduledTaskTrigger `
+                    -User "$env:USERDOMAIN\$env:USERNAME" `
+                    -Password $ScheduledTaskUserPassword `
+                    -Settings $ScheduledTaskSettingsSet
+
+    $Task.Triggers[0].ExecutionTimeLimit = "PT30M"
+    $task.Triggers.Repetition.Duration = "P1D" 
+    $task.Triggers.Repetition.Interval = "PT1M"
+    $task | Set-ScheduledTask -Password $ScheduledTaskUserPassword -User "$env:USERDOMAIN\$env:USERNAME"
+}
+
+function Uninstall-CamundaExternalServiceTaskPowerShellRouter {
+    $Task = Get-ScheduledTask | where taskname -match "Invoke-CamundaExternalServiceTaskPowerShellRouting"
+    $Task | Unregister-ScheduledTask
+}
 
 function Invoke-CamundaExternalServiceTaskPowerShellRouting {
     $CamundaTopics = @()
@@ -41,12 +79,88 @@ function Invoke-CamundaExternalServiceTaskPowerShellRouting {
         }
 
         $PowerShellCommand = $PowerShellFunctionName + " " + $PowerShellFunctionParameters -join " "
-        $PowerShellResult = Invoke-Expression $PowerShellCommand
-    
         $Variables = @{}
-        $Variables += New-CamundaVariable -Name "Response" -Value $PowerShellResult | ConvertTo-HashTable
 
-        Complete-CamundaExternalTask -ExternalTaskID $ExternalServiceTask.id -WorkerID "PowerShell" -Variables $Variables
+        try {
+            $PowerShellResult = Invoke-Expression $PowerShellCommand
+            $Variables += $PowerShellResult | ConvertTo-CamundaVariable | ConvertTo-HashTable
+            Complete-CamundaExternalTask -ExternalTaskID $ExternalServiceTask.id -WorkerID "PowerShell" -Variables $Variables
+        } catch {
+            $ExceptionString = $_.Exception.Message
+            Invoke-CamundaExternalTaskFailure -ExternalTaskID $ExternalServiceTask.id -WorkerID "PowerShell" -ErrorMessage $_.Exception.Message
+        }
+    
+        #$Variables += New-CamundaVariable -Name "Response" -Value $PowerShellResult | ConvertTo-HashTable
+        
+
+        #$PowerShellResult | ConvertTo-CamundaVariable
+
+    }
+}
+
+
+$DotNetToCamundaTypeMappings = [pscustomobject][ordered]@{
+    DotNetTypeName = "System.Int32"
+    CamundaTypeName = "integer"
+},
+[pscustomobject][ordered]@{
+    DotNetTypeName = "System.Int16"
+    CamundaTypeName = "short"
+},
+[pscustomobject][ordered]@{
+    DotNetTypeName = "System.Boolean"
+    CamundaTypeName = "boolean"
+},
+[pscustomobject][ordered]@{
+    DotNetTypeName = "System.Int64"
+    CamundaTypeName = "long"
+},
+[pscustomobject][ordered]@{
+    DotNetTypeName = "System.Double"
+    CamundaTypeName = "double"
+},
+[pscustomobject][ordered]@{
+    DotNetTypeName = "System.DateTime"
+    CamundaTypeName = "date"
+},
+[pscustomobject][ordered]@{
+    DotNetTypeName = "System.String"
+    CamundaTypeName = "string"
+},
+[pscustomobject][ordered]@{
+    DotNetTypeName = "System.Xml.XmlDocument"
+    CamundaTypeName = "XML"
+}
+#,
+#[pscustomobject][ordered]@{
+#    DotNetTypeName = ""
+#    CamundaTypeName = "JSON"
+#},
+#[pscustomobject][ordered]@{
+#    DotNetTypeName = ""
+#    CamundaTypeName = "file"
+#},
+#[pscustomobject][ordered]@{
+#    DotNetTypeName = ""
+#    CamundaTypeName = "bytes"
+#},
+
+function ConvertTo-CamundaVariable {
+    param(
+        [Parameter(Mandatory,ValueFromPipeline)]$InputObject
+    )
+
+    Foreach ($Property in $InputObject.psobject.properties) {
+
+        if ($Property.typenameofvalue -in $DotNetToCamundaTypeMappings.dotnettypename) {
+            $CamundaTypeName = $DotNetToCamundaTypeMappings | 
+            where dotnettypename -eq $Property.typenameofvalue |
+            Select -ExpandProperty CamundaTypeName
+
+            New-CamundaVariable -Name $Property.Name -Value $Property.Value -Type $CamundaTypeName
+        } else {
+            New-CamundaVariable -Name $Property.Name -Value $Property.Value -Type JSON
+        }
     }
 }
 
